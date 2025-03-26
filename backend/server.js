bee161e370037694f32c039fefd6c495869100a4
Sukumar Biswas <sukumarbsws98@gmail.com>
2025-03-26 21:23:12 +0600

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
+
+const app = express();
+
+//Middleware to handle cors
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods : ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
