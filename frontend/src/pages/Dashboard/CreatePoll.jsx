d127722192002fddd6918b47dd24b3bf91d646fd
Sukumar Biswas <sukumarbsws98@gmail.com>
2025-03-28 12:52:33 +0600

    poll image option input component

--- frontend/src/pages/Dashboard/CreatePoll.jsx
+++ frontend/src/pages/Dashboard/CreatePoll.jsx
@@ -1,8 +1,117 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import useUserAuth from '../../hooks/useUserAuth'
+import { UserContext } from '../../context/UserContext'
+import { POLL_TYPE } from '../../utils/data'
+import OptionInput from '../../components/input/OptionInput'
+import OptionImageSelector from '../../components/input/OptionImageSelector'
 
 const CreatePoll = () => {
+  useUserAuth()
+
+  const { user } = useContext(UserContext)
+
+  const [pollData, setPollData] = useState({
+    question: "",
+    type: "",
+    options: [],
+    imageOptions: [],
+
+    error: ""
+  })
+
+  const handleValueChange = (key, value) => {
+    setPollData((prev) => ({
+      ...prev,
+      [key]: value
+    }))
+  }
+
+  //create a new poll
+  const handleCreatePoll = async () => {
+    
+  }
   return (
-    <div>CreatePoll</div>
+    <DashboardLayout activeMenu='Create Poll'>
+      <div className='bg-gray-100/80 my-5 p-5 rounded-lg mx-auto'>
+        <h2 className='text-lg text-black font-medium'>Create poll</h2>
+
+        <div className='mt-3'>
+          <label htmlFor="" className='text-xs font-medium text-slate-600'>QUESTION</label>
+
+          <textarea name="" id=""
+            placeholder="What's in your mind"
+            className='w-full text-[13px] text-black outline-none bg-slate-200/80 p-2 rounded-md mt-2'
+            rows={4}
+            value={pollData.question}
+            onChange={({ target }) =>
+              handleValueChange("question", target.value)
+            }
+          />
+        </div>
+
+        <div className='mt-3'>
+          <label htmlFor="" className='text-xs font-medium text-slate-600'>Poll Type</label>
+
+          <div className='flex gap-4 flex-wrap mt-3'>
+            {POLL_TYPE.map((item)=>{
+              <div key={item.value} className={`option-chip ${
+                pollData.type == item.value
+                ? 'text-white bg-primary border-primary'
+                : "border-sky-100"
+              }`}
+              onClick={handleValueChange("type",item.value)}>
+                {item.label}
+              </div>
+            })}
+          </div>
+        </div>
+
+        {pollData.type === "single-choice" && (
+          <div className='mt-5'>
+            <label htmlFor="" className='text-xs font-medium text-slate-600'>
+              OPTIONS
+            </label>
+
+            <div className='mt-3'>
+              <OptionInput 
+              optionList={pollData.options}
+              setOptionList={(value)=>{
+                handleValueChange("options", value)
+              }}
+              />
+            </div>
+          </div>  
+        )}
+
+        {pollData.type === "image-based" &&(
+          <div className='mt-5'>
+            <label className='text-xs font-medium text-slate-600'>
+              IMAGE OPTIONS
+            </label>
+
+            <div className='mt-3'>
+              <OptionImageSelector
+              imageList={pollData.imageOptions}
+              setImageList={(value)=>{
+                handleValueChange("imageOptions",value)
+              }}
+              />
+            </div>
+          </div>
+        )}
+
+        {pollData.error && (
+          <p className='text-xs font-medium text-red-500 mt-5'>
+            {pollData.error}
+          </p>
+        )}
+
+        <button className='btn-primary py-2 mt-6' onClick={handleCreatePoll}>
+          CREATE
+        </button>
+      </div>
+    </DashboardLayout>
   )
 }
 
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -38,3 +38,11 @@ export const SIDE_MENU_DATA = [
         path : "logout"
     }
 ]
+
+export const POLL_TYPE = [
+    {id: "01", label: "Yes/No", value: "yes/no"},
+    {id: "02", label: "Single Choice", value: "single-choice"},
+    {id: "03", label: "Rating", value: "rating"},
+    {id: "04", label: "Image Based", value: "imaeg-based"},
+    {id: "05", label: "Open Ended", value: "open-ended"},
+]
